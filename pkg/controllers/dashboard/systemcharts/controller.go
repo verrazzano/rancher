d3322376bdeda26fecd4c44d34bb452a5bb67c1a
Guoyong Zhang <guoyong.zhang@oracle.com>
2022-12-19 16:46:16 -0500

    build from source v2.6.8

--- pkg/controllers/dashboard/systemcharts/controller.go
+++ pkg/controllers/dashboard/systemcharts/controller.go
@@ -2,6 +2,7 @@ package systemcharts
 
 import (
 	"context"
+	"os"
 	"sync"
 
 	catalog "github.com/rancher/rancher/pkg/apis/catalog.cattle.io/v1"
@@ -103,10 +104,31 @@ func (h *handler) onRepo(key string, repo *catalog.ClusterRepo) (*catalog.Cluste
 				values[k] = v
 			}
 		}
+
+		if chartDef.ChartName == "rancher-webhook" {
+			overrideRancherWebhookImage(values)
+		}
+
 		if err := h.manager.Ensure(chartDef.ReleaseNamespace, chartDef.ChartName, chartDef.MinVersionSetting.Get(), values, false); err != nil {
 			return repo, err
 		}
 	}
 
 	return repo, nil
+}
+
+// overrideRancherWebhookImage sets Helm chart values to override the Rancher Webhook image name and/or tag based on environment variables.
+func overrideRancherWebhookImage(rancherWebhookChartValues map[string]interface{}) {
+	chartValues := make(map[string]interface{})
+
+	if envVal, ok := os.LookupEnv("RANCHER_WEBHOOK_IMAGE"); ok {
+		chartValues["repository"] = envVal
+	}
+	if envVal, ok := os.LookupEnv("RANCHER_WEBHOOK_IMAGE_TAG"); ok {
+		chartValues["tag"] = envVal
+	}
+
+	if len(chartValues) > 0 {
+		rancherWebhookChartValues["image"] = chartValues
+	}
 }
