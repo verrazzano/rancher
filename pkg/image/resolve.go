cfa03ab4cbc47377eb284f9ff1ea151a14ea31ad
Michael Gianatassio <michael.gianatassio@oracle.com>
2023-01-25 11:17:41 -0800

    fix compile error

--- pkg/image/resolve.go
+++ pkg/image/resolve.go
@@ -43,7 +43,10 @@ func Resolve(image string) string {
 }
 
 func ResolveWithCluster(image string, cluster *v3.Cluster) string {
-	reg := util.GetPrivateRepoURL(cluster)
+	if cluster == nil {
+		return image
+	}
+	reg := util.GetPrivateRegistryURL(cluster.Spec.RancherKubernetesEngineConfig)
 	if reg != "" && !strings.HasPrefix(image, reg) {
 		/*
 			Separating the image from the default registry url through split
