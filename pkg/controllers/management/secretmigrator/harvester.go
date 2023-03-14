416f8cf643ad72cfae16ed2969895a5c65d1e806
Robert Donat <robert.donat@oracle.com>
2023-03-14 11:42:59 -0400

    fix compilation failure

---
+++ pkg/controllers/management/secretmigrator/harvester.go
@@ -0,0 +1,6 @@
+package secretmigrator
+
+const (
+	AuthorizedSecretAnnotation                        = "v2prov-secret-authorized-for-cluster"
+	AuthorizedSecretDeletesOnClusterRemovalAnnotation = "v2prov-authorized-secret-deletes-on-cluster-removal"
+)
