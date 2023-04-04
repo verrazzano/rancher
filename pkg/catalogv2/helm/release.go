56b9dc91391073aa12764262d0f12d7ec06a1096
Marco Gellecanao <marco.m.gellecanao@oracle.com>
2023-04-04 10:57:41 -0700

    VZ-9028: BFS, remove Helm V2 (#22)

--- pkg/catalogv2/helm/release.go
+++ pkg/catalogv2/helm/release.go
@@ -69,11 +69,8 @@ func ToRelease(obj runtime.Object, isNamespaced IsNamespaced) (*v1.ReleaseSpec, 
 		return nil, err
 	}
 
-	switch {
-	case isHelm3(meta.GetLabels()):
+	if isHelm3(meta.GetLabels()) {
 		return fromHelm3Data(releaseData, isNamespaced)
-	case isHelm2(meta.GetLabels()):
-		return fromHelm2Data(releaseData, isNamespaced)
 	}
 
 	return nil, ErrNotHelmRelease
