3116589aa73feee4f27ee64fbcdc3906107c01fc
Abhishek Mitra <abhishek.a.mitra@oracle.com>
2023-04-17 12:58:41 -0700

    VZ-9205-Decouple node drivers and cloud creds (#37)
    * VZ-9205: Add back rancher machine
    
    * VZ-9205: Add badic node drivers
    
    * VZ-9205: Fix import
    
    * Remove RKE and add node drivers as inactive
    
    * Fix cattle machine version
    
    * Create dynamic schema independant of node drivers
    
    * Remove debug and update azure
    
    * remove single debug line
    
    * VZ-9205: Remove the machine driver binary and comments
    
    * VZ-9205: Remove error string
    
    * VZ-9205: Remove locks
    
    * VZ-9205: Remove node driver related specs
    
    * VZ-9205: Add region support to OCI
    
    * VZ-9205: Add OCI region support to OCI secret

--- pkg/data/management/kontainerdriver_data.go
+++ pkg/data/management/kontainerdriver_data.go
@@ -31,10 +31,6 @@ func addKontainerDrivers(management *config.ManagementContext) error {
 		return err
 	}
 
-	if err := creator.add("rancherKubernetesEngine"); err != nil {
-		return err
-	}
-
 	if err := creator.add("googleKubernetesEngine"); err != nil {
 		return err
 	}
