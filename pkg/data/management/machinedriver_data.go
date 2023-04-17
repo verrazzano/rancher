fe96a34d160b3fc2ad37a719f0f50f1a1daf53f0
Abhishek Mitra <abhishek.a.mitra@oracle.com>
2023-04-17 12:06:40 -0700

    VZ-9205: Add OCI region support to OCI secret

--- pkg/data/management/machinedriver_data.go
+++ pkg/data/management/machinedriver_data.go
@@ -55,7 +55,7 @@ var (
 		HarvesterDriver:    {"publicCredentialFields": []string{"clusterType", "clusterId"}, "privateCredentialFields": []string{"kubeconfigContent"}, "optionalCredentialFields": []string{"clusterId"}},
 		Linodedriver:       {"privateCredentialFields": []string{"token"}, "passwordFields": []string{"rootPass"}},
 		NutanixDriver:      {"publicCredentialFields": []string{"endpoint", "username", "port"}, "privateCredentialFields": []string{"password"}},
-		OCIDriver:          {"publicCredentialFields": []string{"tenancyId", "userId", "fingerprint"}, "privateCredentialFields": []string{"privateKeyContents"}, "passwordFields": []string{"privateKeyPassphrase"}},
+		OCIDriver:          {"publicCredentialFields": []string{"tenancyId", "userId", "fingerprint", "region"}, "privateCredentialFields": []string{"privateKeyContents"}, "passwordFields": []string{"privateKeyPassphrase"}},
 		OTCDriver:          {"privateCredentialFields": []string{"accessKeySecret"}},
 		OpenstackDriver:    {"privateCredentialFields": []string{"password"}},
 		PacketDriver:       {"privateCredentialFields": []string{"apiKey"}},
