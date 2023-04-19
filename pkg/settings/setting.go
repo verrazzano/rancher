3ee1216267fb798db3ebe36a39e86eb31b19ba0f
Abhishek Mitra <abhishek.a.mitra@oracle.com>
2023-04-19 14:01:36 -0700

    Update env value of shell

--- pkg/settings/setting.go
+++ pkg/settings/setting.go
@@ -93,7 +93,7 @@ var (
 	PartnerChartDefaultBranch           = NewSetting("partner-chart-default-branch", "main")
 	RKE2ChartDefaultBranch              = NewSetting("rke2-chart-default-branch", "main")
 	FleetDefaultWorkspaceName           = NewSetting("fleet-default-workspace-name", fleetconst.ClustersDefaultNamespace) // fleetWorkspaceName to assign to clusters with none
-	ShellImage                          = NewSetting("shell-image", "rancher/shell:v0.1.18")
+	ShellImage                          = NewSetting("shell-image", GetEnvWithDefault("CATTLE_SHELL_IMAGE", "rancher/shell:v0.1.18"))
 	IgnoreNodeName                      = NewSetting("ignore-node-name", "") // nodes to ignore when syncing v1.node to v3.node
 	NoDefaultAdmin                      = NewSetting("no-default-admin", "")
 	RestrictedDefaultAdmin              = NewSetting("restricted-default-admin", "false") // When bootstrapping the admin for the first time, give them the global role restricted-admin
@@ -395,4 +395,13 @@ func GetRancherVersion() string {
 		return RancherVersionDev
 	}
 	return strings.TrimPrefix(rancherVersion, "v")
+}
+
+// GetEnvWithDefault returns if a value is set in env, else the default value
+func GetEnvWithDefault(key, defaultValue string) string {
+	value, ok := os.LookupEnv(key)
+	if !ok {
+		return defaultValue
+	}
+	return value
 }
