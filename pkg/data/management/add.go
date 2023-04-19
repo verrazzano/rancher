0d8500a705c2774cbb8536a5afb1c19aa7639ad0
Abhishek Mitra <abhishek.a.mitra@oracle.com>
2023-04-19 15:06:45 -0700

    VZ-9205 cloud creds retest (#51)
    * VZ-9205: Add back rancher machine
    
    * VZ-9205: Add badic node drivers
    
    * VZ-9205: Fix import
    
    * Remove RKE and add node drivers as inactive
    
    * Fix cattle machine version
    
    * Create dynamic schema independant of node drivers
    
    * Remove debug and update azure
    
    * remove single debug line
    
    * VZ-9205: Remove the machine driver binary and comments
    
    * VZ-9205: Remove error string
    
    * VZ-9205: Remove locks
    
    * VZ-9205: Remove node driver related specs
    
    * VZ-9205: Add region support to OCI
    
    * VZ-9205: Add OCI region support to OCI secret
    
    * Add back RKE cluster driver
    
    * Revert cleanup

--- pkg/data/management/add.go
+++ pkg/data/management/add.go
@@ -37,5 +37,9 @@ func Add(ctx context.Context, wrangler *wrangler.Context, management *config.Man
 		return err
 	}
 
-	return addCattleGlobalNamespaces(management)
+	if err := addCattleGlobalNamespaces(management); err != nil {
+		return err
+	}
+
+	return addCloudCredentials(management)
 }
