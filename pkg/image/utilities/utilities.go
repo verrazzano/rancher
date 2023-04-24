94dc6022a6462dbf0ec1aee20d2f3e1659bc3f58
Abhishek Mitra <abhishek.a.mitra@oracle.com>
2023-04-24 16:45:30 -0700

    Remove check for ghcr

--- pkg/image/utilities/utilities.go
+++ pkg/image/utilities/utilities.go
@@ -280,7 +280,7 @@ func saveImagesAndSources(imagesAndSources []string) []string {
 
 func checkImage(image string) error {
 	// ignore non prefixed images, also in types (image/mirror.go)
-	if strings.HasPrefix(image, "weaveworks") || strings.HasPrefix(image, "noiro") || strings.HasPrefix(image, "ghcr") {
+	if strings.HasPrefix(image, "weaveworks") || strings.HasPrefix(image, "noiro") {
 		return nil
 	}
 	imageNameTag := strings.Split(image, ":")
