e6875d1795f7b20281729a3faec1e9c0369b5eec
Abhishek Mitra <abhishek.a.mitra@oracle.com>
2023-04-24 08:16:02 -0700

    VZ-9037: shell env update

--- pkg/image/utilities/utilities.go
+++ pkg/image/utilities/utilities.go
@@ -280,7 +280,7 @@ func saveImagesAndSources(imagesAndSources []string) []string {
 
 func checkImage(image string) error {
 	// ignore non prefixed images, also in types (image/mirror.go)
-	if strings.HasPrefix(image, "weaveworks") || strings.HasPrefix(image, "noiro") {
+	if strings.HasPrefix(image, "weaveworks") || strings.HasPrefix(image, "noiro") || strings.HasPrefix(image, "ghcr") {
 		return nil
 	}
 	imageNameTag := strings.Split(image, ":")
