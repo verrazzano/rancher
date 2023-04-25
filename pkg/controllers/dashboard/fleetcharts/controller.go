7175c793cf756ae2921fc39f81652c3be8ac656a
Michael Gianatassio <michael.gianatassio@oracle.com>
2023-04-25 06:57:14 -0700

    trying to fix fleetcharts/controller.go

--- pkg/controllers/dashboard/fleetcharts/controller.go
+++ pkg/controllers/dashboard/fleetcharts/controller.go
@@ -75,7 +75,7 @@ func (h *handler) onSetting(key string, setting *v3.Setting) (*v3.Setting, error
 	}
 	h.Unlock()
 
-	err := h.manager.Ensure(fleetCRDChart.ReleaseNamespace, fleetCRDChart.ChartName, settings.FleetMinVersion.Get(), nil, true)
+	err := h.manager.Ensure(fleetCRDChart.ReleaseNamespace, fleetCRDChart.ChartName, settings.FleetMinVersion.Get(), nil, true, "")
 	if err != nil {
 		return setting, err
 	}
@@ -128,7 +128,7 @@ func (h *handler) onSetting(key string, setting *v3.Setting) (*v3.Setting, error
 		fleetChartValues["gitjob"] = gitjobChartValues
 	}
 
-	return setting, h.manager.Ensure(fleetChart.ReleaseNamespace, fleetChart.ChartName, settings.FleetMinVersion.Get(), fleetChartValues, true)
+	return setting, h.manager.Ensure(fleetChart.ReleaseNamespace, fleetChart.ChartName, settings.FleetMinVersion.Get(), fleetChartValues, true, "")
 }
 
 // overrideFleetImages overrides the Fleet image names and/or tags
