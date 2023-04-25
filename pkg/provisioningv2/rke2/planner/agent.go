30c0def29b7450649df566981cecda9cbaf43192
Michael Gianatassio <michael.gianatassio@oracle.com>
2023-04-25 09:53:01 -0700

    save

--- pkg/provisioningv2/rke2/planner/agent.go
+++ pkg/provisioningv2/rke2/planner/agent.go
@@ -5,10 +5,7 @@ import (
 	"sort"
 
 	rkev1 "github.com/rancher/rancher/pkg/apis/rke.cattle.io/v1"
-	util "github.com/rancher/rancher/pkg/cluster"
-	"github.com/rancher/rancher/pkg/controllers/management/secretmigrator"
 	"github.com/rancher/rancher/pkg/systemtemplate"
-	rketypes "github.com/rancher/rke/types"
 )
 
 // generateClusterAgentManifest generates a cluster agent manifest
@@ -40,24 +37,5 @@ func (p *Planner) generateClusterAgentManifest(controlPlane *rkev1.RKEControlPla
 		return nil, err
 	}
 
-	// assemble private registry
-	var registry *rketypes.PrivateRegistry
-	privateRegistrySecretRef := mgmtCluster.GetSecret("PrivateRegistrySecret")
-	privateRegistryURL := mgmtCluster.Spec.ClusterSecrets.PrivateRegistryURL
-	ecrSecretRef := mgmtCluster.Spec.ClusterSecrets.PrivateRegistryECRSecret
-	if privateRegistrySecretRef != "" || privateRegistryURL != "" || ecrSecretRef != "" {
-		// Assemble private registry secrets here instead of directly on the cluster object
-		spec := *mgmtCluster.Spec.DeepCopy()
-		spec, err = secretmigrator.AssemblePrivateRegistryCredential(privateRegistrySecretRef, privateRegistryURL, secretmigrator.ClusterType, mgmtCluster.Name, spec, p.secretCache)
-		if err != nil {
-			return nil, err
-		}
-		spec, err = secretmigrator.AssemblePrivateRegistryECRCredential(ecrSecretRef, secretmigrator.ClusterType, mgmtCluster.Name, mgmtCluster.Spec, p.secretCache)
-		if err != nil {
-			return nil, err
-		}
-		registry = util.GetPrivateRegistry(spec.RancherKubernetesEngineConfig)
-	}
-
-	return systemtemplate.ForCluster(mgmtCluster, tokens[0].Status.Token, registry, taints)
+	return systemtemplate.ForCluster(mgmtCluster, tokens[0].Status.Token, taints, p.secretCache)
 }
