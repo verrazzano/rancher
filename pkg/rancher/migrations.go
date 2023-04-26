4e36d98d314fe9442a84df41169a83f8119a548f
Michael Gianatassio <michael.gianatassio@oracle.com>
2023-04-26 11:01:41 -0700

    fix some merge conflict errors

--- pkg/rancher/migrations.go
+++ pkg/rancher/migrations.go
@@ -33,19 +33,17 @@ import (
 )
 
 const (
-	cattleNamespace                            = "cattle-system"
-	forceUpgradeLogoutConfig                   = "forceupgradelogout"
-	forceLocalSystemAndDefaultProjectCreation  = "forcelocalprojectcreation"
-	forceSystemNamespacesAssignment            = "forcesystemnamespaceassignment"
-	migrateFromMachineToPlanSecret             = "migratefrommachinetoplanesecret"
-	migrateEncryptionKeyRotationLeaderToStatus = "migrateencryptionkeyrotationleadertostatus"
-	migrateDynamicSchemaToMachinePools         = "migratedynamicschematomachinepools"
-	rancherVersionKey                          = "rancherVersion"
-	projectsCreatedKey                         = "projectsCreated"
-	namespacesAssignedKey                      = "namespacesAssigned"
-	capiMigratedKey                            = "capiMigrated"
-	encryptionKeyRotationStatusMigratedKey     = "encryptionKeyRotationStatusMigrated"
-	dynamicSchemaMachinePoolsMigratedKey       = "dynamicSchemaMachinePoolsMigrated"
+	cattleNamespace                           = "cattle-system"
+	forceUpgradeLogoutConfig                  = "forceupgradelogout"
+	forceLocalSystemAndDefaultProjectCreation = "forcelocalprojectcreation"
+	forceSystemNamespacesAssignment           = "forcesystemnamespaceassignment"
+	migrateFromMachineToPlanSecret            = "migratefrommachinetoplanesecret"
+	migrateDynamicSchemaToMachinePools        = "migratedynamicschematomachinepools"
+	rancherVersionKey                         = "rancherVersion"
+	projectsCreatedKey                        = "projectsCreated"
+	namespacesAssignedKey                     = "namespacesAssigned"
+	capiMigratedKey                           = "capiMigrated"
+	dynamicSchemaMachinePoolsMigratedKey      = "dynamicSchemaMachinePoolsMigrated"
 )
 
 func runMigrations(wranglerContext *wrangler.Context) error {
@@ -65,18 +63,6 @@ func runMigrations(wranglerContext *wrangler.Context) error {
 
 	if features.EmbeddedClusterAPI.Enabled() {
 		if err := addWebhookConfigToCAPICRDs(wranglerContext.CRD.CustomResourceDefinition()); err != nil {
-			return err
-		}
-	}
-
-	if features.RKE2.Enabled() {
-		if err := migrateCAPIMachineLabelsAndAnnotationsToPlanSecret(wranglerContext); err != nil {
-			return err
-		}
-		if err := migrateEncryptionKeyRotationLeader(wranglerContext); err != nil {
-			return err
-		}
-		if err := migrateMachinePoolsDynamicSchemaLabel(wranglerContext); err != nil {
 			return err
 		}
 	}
@@ -392,56 +378,6 @@ func migrateCAPIMachineLabelsAndAnnotationsToPlanSecret(w *wrangler.Context) err
 	}
 
 	cm.Data[capiMigratedKey] = "true"
-	return createOrUpdateConfigMap(w.Core.ConfigMap(), cm)
-}
-
-func migrateEncryptionKeyRotationLeader(w *wrangler.Context) error {
-	cm, err := getConfigMap(w.Core.ConfigMap(), migrateEncryptionKeyRotationLeaderToStatus)
-	if err != nil || cm == nil {
-		return err
-	}
-
-	if cm.Data[encryptionKeyRotationStatusMigratedKey] == "true" {
-		return nil
-	}
-
-	mgmtClusters, err := w.Mgmt.Cluster().List(metav1.ListOptions{})
-	if err != nil {
-		return err
-	}
-
-	for _, mgmtCluster := range mgmtClusters.Items {
-		if err = retry.RetryOnConflict(retry.DefaultRetry, func() error {
-			cp, err := w.RKE.RKEControlPlane().Get(mgmtCluster.Spec.FleetWorkspaceName, mgmtCluster.Name, metav1.GetOptions{})
-			if k8serror.IsNotFound(err) {
-				return nil
-			}
-			if err != nil {
-				return err
-			}
-			leader := cp.Annotations["rke.cattle.io/encrypt-key-rotation-leader"]
-			if leader == "" {
-				return nil
-			}
-			cp = cp.DeepCopy()
-			cp.Status.RotateEncryptionKeysLeader = leader
-			cp, err = w.RKE.RKEControlPlane().UpdateStatus(cp)
-			if err != nil {
-				return err
-			}
-			cp = cp.DeepCopy()
-			delete(cp.Annotations, "rke.cattle.io/encrypt-key-rotation-leader")
-			cp, err = w.RKE.RKEControlPlane().Update(cp)
-			if err != nil {
-				return err
-			}
-			return nil
-		}); err != nil {
-			return err
-		}
-	}
-
-	cm.Data[encryptionKeyRotationStatusMigratedKey] = "true"
 	return createOrUpdateConfigMap(w.Core.ConfigMap(), cm)
 }
 
