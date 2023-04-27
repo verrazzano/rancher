ef656dcdfd8093e1bf3c08d534fec6040299353b
Michael Gianatassio <31552226+mgianatagh@users.noreply.github.com>
2023-04-27 10:16:30 -0700

    VZ-9468: Revert to using rancher/charts  (#71)
    VZ-9468: Revert to using rancher/charts

--- pkg/data/dashboard/repo.go
+++ pkg/data/dashboard/repo.go
@@ -2,14 +2,17 @@ package dashboard
 
 import (
 	"context"
-
-	apierrors "k8s.io/apimachinery/pkg/api/errors"
-
-	"github.com/rancher/rancher/pkg/settings"
+	"strings"
 
 	v1 "github.com/rancher/rancher/pkg/apis/catalog.cattle.io/v1"
+	"github.com/rancher/rancher/pkg/settings"
 	"github.com/rancher/rancher/pkg/wrangler"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+var (
+	prefix = "rancher-"
 )
 
 func addRepo(wrangler *wrangler.Context, repoName, branchName string) error {
@@ -20,7 +23,7 @@ func addRepo(wrangler *wrangler.Context, repoName, branchName string) error {
 				Name: repoName,
 			},
 			Spec: v1.RepoSpec{
-				GitRepo:   "https://github.com/verrazzano/" + repoName,
+				GitRepo:   "https://git.rancher.io/" + strings.TrimPrefix(repoName, prefix),
 				GitBranch: branchName,
 			},
 		})
