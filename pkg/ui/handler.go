8aaa853747b28402f4c1f756b74a194f44ea3119
Michael Gianatassio <michael.gianatassio@oracle.com>
2023-05-05 15:40:46 -0700

    save

--- pkg/ui/handler.go
+++ pkg/ui/handler.go
@@ -20,7 +20,7 @@ var (
 
 	ember = newHandler(settings.UIIndex.Get,
 		settings.UIPath.Get,
-		settings.UIOfflinePreferred.Get)
+		func() string { return "true" })
 	emberAlwaysOffline = newHandler(settings.UIIndex.Get,
 		settings.UIPath.Get,
 		func() string { return "true" })
