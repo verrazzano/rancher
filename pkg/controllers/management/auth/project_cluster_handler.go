ef37097ab820ca8defa60deab1c63e54e382fd93
Abijitha Nadagouda <29689969+Abhijithang@users.noreply.github.com>
2023-05-25 04:30:28 -0700

    Refactor

--- pkg/controllers/management/auth/project_cluster_handler.go
+++ pkg/controllers/management/auth/project_cluster_handler.go
@@ -517,10 +517,9 @@ func (m *mgr) deleteNamespace(obj runtime.Object, controller string) error {
 			if apierrors.IsNotFound(err) {
 				logrus.Infof("Cluster NAME NOT FOUND: ------%v", c.GetName())
 				break
-			} else {
-				logrus.Infof("In LOOP: ERROR: ------%v", c.GetName())
-				return err
 			}
+			logrus.Infof("In LOOP: ERROR: ------%v", c.GetName())
+			return err
 		}
 	}
 
