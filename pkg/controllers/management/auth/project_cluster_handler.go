ef6442533e321c56bd06b0dfb212e3d6baa8ee23
Abijitha Nadagouda <29689969+Abhijithang@users.noreply.github.com>
2023-05-25 11:57:22 -0700

    REfactor fix

--- pkg/controllers/management/auth/project_cluster_handler.go
+++ pkg/controllers/management/auth/project_cluster_handler.go
@@ -502,26 +502,23 @@ func (m *mgr) deleteNamespace(obj runtime.Object, controller string) error {
 		return nil
 	}
 
-	//m.mgmt.Management.Clusters("c-dbvs6  ").Get()
 	sleep := 60
 	for i := 0; i <= 3; i++ {
 		c, err := m.mgmt.Management.Clusters("").Get(o.GetName(), v1.GetOptions{})
 		logrus.Infof("CLUSTER NAME THAT IS BEING DELETED IS: --------%v", c.GetName())
-
-		if !apierrors.IsNotFound(err) {
-			time.Sleep(time.Duration(sleep) * time.Second)
-			sleep *= 2
-			continue
-		}
 		if err != nil {
 			if apierrors.IsNotFound(err) {
 				logrus.Infof("Cluster NAME NOT FOUND: ------%v", c.GetName())
+				logrus.Infof("BREAKING THE LOOP------: ------%v", c.GetName())
 				break
 			} else {
 				logrus.Infof("In LOOP: ERROR: ------%v", c.GetName())
 				return err
 			}
 		}
+		logrus.Infof("SLEEPING-------------: --------%v", c.GetName())
+		time.Sleep(time.Duration(sleep) * time.Second)
+		continue
 	}
 
 	if ns.Status.Phase != v12.NamespaceTerminating {
