e23f82603c1f8d6c4e90171cdef51cf59e556095
Abijitha Nadagouda <29689969+Abhijithang@users.noreply.github.com>
2023-05-26 08:16:18 -0700

    Refactor the logs and add comments

--- pkg/controllers/management/auth/project_cluster_handler.go
+++ pkg/controllers/management/auth/project_cluster_handler.go
@@ -504,17 +504,17 @@ func (m *mgr) deleteNamespace(obj runtime.Object, controller string) error {
 
 	sleep := 10         // sleep interval in seconds
 	pollDuration := 600 // Total poll duration in seconds
+	//  Polls to verify if the cluster exists every 10 seconds for a total duration of 10 minutes.
 	for i := 0; i < pollDuration/sleep; i++ {
 		c, err := m.mgmt.Wrangler.CAPI.Cluster().Get(o.GetName(), o.GetName(), v1.GetOptions{})
-		logrus.Infof("Cluster:%v/%v", o.GetName(), o.GetNamespace())
 		if err != nil {
 			if apierrors.IsNotFound(err) {
-				logrus.Infof("Cluster not found:%v/%v", c.GetName(), c.GetNamespace())
+				logrus.Infof("Cluster not found:%v/%v", c.GetName(), c.GetName())
 				break
 			}
 			return err
 		}
-		logrus.Infof("Waiting for Cluster to be deleted..%v/%v", c.GetName(), c.GetNamespace())
+		logrus.Infof("Waiting for Cluster to be deleted..%v/%v", c.GetName(), c.GetName())
 		time.Sleep(time.Duration(sleep) * time.Second)
 	}
 
