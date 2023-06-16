2162a0c24ab4b24741b41b4372cb259dc8ad65a1
Anders Swanson <91502735+anders-swanson@users.noreply.github.com>
2023-06-16 13:02:52 -0700

    Clean up CAPI cluster on delete, if exists. (#111)
    CAPI Cluster cleanup

--- pkg/controllers/management/auth/project_cluster_handler.go
+++ pkg/controllers/management/auth/project_cluster_handler.go
@@ -502,6 +502,26 @@ func (m *mgr) deleteNamespace(obj runtime.Object, controller string) error {
 		return nil
 	}
 
+	if err := m.cleanupCAPICluster(ns, o); err != nil {
+		return err
+	}
+
+	if ns.Status.Phase != v12.NamespaceTerminating {
+		logrus.Infof("[%v] Deleting namespace %v", controller, o.GetName())
+		err = nsClient.Delete(context.TODO(), o.GetName(), v1.DeleteOptions{})
+		if apierrors.IsNotFound(err) {
+			return nil
+		}
+	}
+	return err
+}
+
+func (m *mgr) cleanupCAPICluster(ns *v12.Namespace, o v1.Object) error {
+	err := m.mgmt.Wrangler.CAPI.Cluster().Delete(ns.Name, o.GetName(), &v1.DeleteOptions{})
+	if err != nil && !apierrors.IsNotFound(err) {
+		return err
+	}
+
 	for {
 		c, err := m.mgmt.Wrangler.CAPI.Cluster().Get(ns.Name, o.GetName(), v1.GetOptions{})
 		if err != nil {
@@ -514,15 +534,7 @@ func (m *mgr) deleteNamespace(obj runtime.Object, controller string) error {
 		logrus.Infof("Waiting for Cluster to be deleted:%v/%v", ns.Name, c.GetName())
 		time.Sleep(time.Duration(10) * time.Second)
 	}
-
-	if ns.Status.Phase != v12.NamespaceTerminating {
-		logrus.Infof("[%v] Deleting namespace %v", controller, o.GetName())
-		err = nsClient.Delete(context.TODO(), o.GetName(), v1.DeleteOptions{})
-		if apierrors.IsNotFound(err) {
-			return nil
-		}
-	}
-	return err
+	return nil
 }
 
 func (m *mgr) reconcileResourceToNamespace(obj runtime.Object, controller string) (runtime.Object, error) {
