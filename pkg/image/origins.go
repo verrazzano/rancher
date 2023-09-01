65e677debcf5a91ae21b4306dd6fa1213f1efe01
Michael Gianatassio <michael.gianatassio@oracle.com>
2023-09-01 16:19:37 -0400

    remove duplicate key

--- pkg/image/origins.go
+++ pkg/image/origins.go
@@ -176,8 +176,6 @@ var OriginMap = map[string]string{
 	"mirrored-neuvector-controller":                           "https://github.com/neuvector/neuvector",
 	"mirrored-neuvector-enforcer":                             "https://github.com/neuvector/neuvector",
 	"mirrored-neuvector-manager":                              "https://github.com/neuvector/manager",
-	"mirrored-neuvector-prometheus-exporter":                  "https://github.com/neuvector/registry-adapter",
-	"mirrored-neuvector-registry-adapter":                     "https://github.com/neuvector/scanner",
 	"mirrored-neuvector-scanner":                              "https://github.com/neuvector/scanner",
 	"mirrored-neuvector-updater":                              "https://github.com/neuvector/neuvector",
 	"mirrored-neuvector-prometheus-exporter":                  "https://github.com/neuvector/prometheus-exporter",
