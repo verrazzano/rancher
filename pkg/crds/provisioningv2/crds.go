bdd634874ef51ad3ee5efa52d7b369e3d1d41447
Michael Gianatassio <michael.gianatassio@oracle.com>
2023-09-19 15:20:45 -0400

    Remove functions that were not being referenced

--- pkg/crds/provisioningv2/crds.go
+++ pkg/crds/provisioningv2/crds.go
@@ -4,7 +4,6 @@ import (
 	"embed"
 
 	v1 "github.com/rancher/rancher/pkg/apis/provisioning.cattle.io/v1"
-	rkev1 "github.com/rancher/rancher/pkg/apis/rke.cattle.io/v1"
 	"github.com/rancher/rancher/pkg/features"
 	"github.com/rancher/wrangler/pkg/crd"
 	"github.com/rancher/wrangler/pkg/data"
@@ -44,68 +43,6 @@ func provisioning() []crd.CRD {
 			return c.
 				WithColumn("Ready", ".status.ready").
 				WithColumn("Kubeconfig", ".status.clientSecretName")
-		}),
-	}
-}
-
-func clusterIndexed(c crd.CRD) crd.CRD {
-	newLabels := map[string]string{}
-	for k, v := range c.Labels {
-		newLabels[k] = v
-	}
-	newLabels["auth.cattle.io/cluster-indexed"] = "true"
-	c.Labels = newLabels
-	return c
-}
-
-func rke2() []crd.CRD {
-	return []crd.CRD{
-		newRancherCRD(&v1.Cluster{}, func(c crd.CRD) crd.CRD {
-			return clusterIndexed(c).
-				WithColumn("Ready", ".status.ready").
-				WithColumn("Kubeconfig", ".status.clientSecretName")
-		}),
-		newRKECRD(&rkev1.RKECluster{}, func(c crd.CRD) crd.CRD {
-			c.Labels = map[string]string{
-				"cluster.x-k8s.io/v1beta1": "v1",
-			}
-			return clusterIndexed(c)
-		}),
-		newRKECRD(&rkev1.RKEControlPlane{}, func(c crd.CRD) crd.CRD {
-			c.Labels = map[string]string{
-				"cluster.x-k8s.io/v1beta1": "v1",
-			}
-			return clusterIndexed(c)
-		}),
-		newRKECRD(&rkev1.RKEBootstrap{}, func(c crd.CRD) crd.CRD {
-			c.Labels = map[string]string{
-				"cluster.x-k8s.io/v1beta1": "v1",
-			}
-			return clusterIndexed(c)
-		}),
-		newRKECRD(&rkev1.RKEBootstrapTemplate{}, func(c crd.CRD) crd.CRD {
-			c.Labels = map[string]string{
-				"cluster.x-k8s.io/v1beta1": "v1",
-			}
-			return clusterIndexed(c)
-		}),
-		newRKECRD(&rkev1.RKEControlPlane{}, func(c crd.CRD) crd.CRD {
-			c.Labels = map[string]string{
-				"cluster.x-k8s.io/v1beta1": "v1",
-			}
-			return clusterIndexed(c)
-		}),
-		newRKECRD(&rkev1.CustomMachine{}, func(c crd.CRD) crd.CRD {
-			c.Labels = map[string]string{
-				"cluster.x-k8s.io/v1beta1": "v1",
-			}
-			return clusterIndexed(c)
-		}),
-		newRKECRD(&rkev1.ETCDSnapshot{}, func(c crd.CRD) crd.CRD {
-			c.Labels = map[string]string{
-				"cluster.x-k8s.io/v1beta1": "v1",
-			}
-			return clusterIndexed(c)
 		}),
 	}
 }
@@ -164,21 +101,6 @@ func capi() []crd.CRD {
 	}
 
 	return result
-}
-
-func newRKECRD(obj interface{}, customize func(crd.CRD) crd.CRD) crd.CRD {
-	crd := crd.CRD{
-		GVK: schema.GroupVersionKind{
-			Group:   "rke.cattle.io",
-			Version: "v1",
-		},
-		Status:       true,
-		SchemaObject: obj,
-	}
-	if customize != nil {
-		crd = customize(crd)
-	}
-	return crd
 }
 
 func newRancherCRD(obj interface{}, customize func(crd.CRD) crd.CRD) crd.CRD {
